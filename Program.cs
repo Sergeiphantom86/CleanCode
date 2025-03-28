b41180e1b5d1b896f2049e107796c2459694b0d0
SERGEY <96051914+Sergeiphantom86@users.noreply.github.com>
2025-03-28 18:47:39 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
- public static int GetValidValue(int value, int min, int max)
+ public static int Clamp(int value, int min, int max)
  {
+     if (min > max)
+     {
+         throw new ArgumentException($"Недопустимый диапазон: {nameof(min)} ({min}) не может быть больше, чем {nameof(max)} ({max})");
+     }
+
      if (value < min)
          return min;
      else if (value > max)
